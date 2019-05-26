b0d7a8408c2f6d911023d702e4958b9ceea8a1e7
Yoann Berenguer <38462796+yoyoberenguer@users.noreply.github.com>
2019-05-26 11:56:47 +0100

    Add files via upload

--- Radar.py
+++ Radar.py
@@ -1,3 +1,28 @@
+# encoding: utf-8
+"""
+
+                   GNU GENERAL PUBLIC LICENSE
+
+                       Version 3, 29 June 2007
+
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+
+ Everyone is permitted to copy and distribute verbatim copies
+
+ of this license document, but changing it is not allowed.
+ """
+
+__author__ = "Yoann Berenguer"
+__copyright__ = "Copyright 2007, Cobra Project"
+__credits__ = ["Yoann Berenguer"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Yoann Berenguer"
+__email__ = "yoyoberenguer@hotmail.com"
+__status__ = "Demo"
+
+
 import numpy
 
 import pygame
@@ -6,6 +31,54 @@ from math import cos, sin, sqrt, atan2
 import os
 from pygame import freetype
 import SoundServer
+
+
+class Halo(pygame.sprite.Sprite):
+    """
+    Create a Halo sprite
+    """
+
+    images = []
+    containers = None
+
+    def __init__(self,
+                 rect_,
+                 timing_,
+                 layer_: int = 0
+                 ):
+        """
+        :param rect_: pygame.Rect representing the coordinates and sprite center
+        :param timing_: integer representing the sprite refreshing time in ms
+        :param layer_: Layer to use, default 0
+        """
+        pygame.sprite.Sprite.__init__(self, self.containers)
+
+        if isinstance(GL.All, pygame.sprite.LayeredUpdates):
+            GL.All.change_layer(self, layer_)
+
+        self.images_copy = self.images.copy()
+        self.image = self.images_copy[0]
+        self.center = rect_.center
+        self.rect = self.image.get_rect(center=self.center)
+        self._blend = False     # blend mode
+        self.dt = 0             # time constant
+        self.index = 0          # list index
+        self.timing = timing_
+
+    def update(self):
+
+        if self.dt > self.timing:
+
+            self.image = self.images_copy[self.index]
+            self.rect = self.image.get_rect(center=self.center)
+            if self.index < len(self.images_copy) - 1:
+                self.index += 1
+            else:
+                self.kill()
+
+            self.dt = 0
+
+        self.dt += GL.TIME_PASSED_SECONDS
 
 
 def make_echo(array_):
@@ -116,6 +189,116 @@ def spread_sheet_per_pixel(file_: str,
     return animation
 
 
+def load_per_pixel(file: str) -> pygame.Surface:
+    """ Not compatible with 8 bit depth color surface"""
+
+    assert isinstance(file, str), 'Expecting path for argument <file> got %s: ' % type(file)
+    try:
+        surface = pygame.image.load(file)
+        buffer_ = surface.get_view('2')
+        w, h = surface.get_size()
+        source_array = numpy.frombuffer(buffer_, dtype=numpy.uint8).reshape((w, h, 4))
+
+        surface_ = pygame.image.frombuffer(source_array.copy(order='C'),
+                                   (tuple(source_array.shape[:2])), 'RGBA').convert_alpha()
+        return surface_
+    except pygame.error:
+        raise SystemExit('\n[-] Error : Could not load image %s %s ' % (file, pygame.get_error()))
+
+
+def make_array(rgb_array_: numpy.ndarray, alpha_: numpy.ndarray) -> numpy.ndarray:
+    """
+    This function is used for 24-32 bit pygame surface with pixel alphas transparency layer
+
+    make_array(RGB array, alpha array) -> RGBA array
+
+    Return a 3D numpy (numpy.uint8) array representing (R, G, B, A)
+    values of all pixels in a pygame surface.
+
+    :param rgb_array_: 3D array that directly references the pixel values in a Surface.
+                       Only work on Surfaces that have 24-bit or 32-bit formats.
+    :param alpha_:     2D array that directly references the alpha values (degree of transparency) in a Surface.
+                       alpha_ is created from a 32-bit Surfaces with a per-pixel alpha value.
+    :return:           Return a numpy 3D array (numpy.uint8) storing a transparency value for every pixel
+                       This allow the most precise transparency effects, but it is also the slowest.
+                       Per pixel alphas cannot be mixed with pygame method set_colorkey (this will have
+                       no effect).
+    """
+    return numpy.dstack((rgb_array_, alpha_))
+
+
+def make_surface(rgba_array: numpy.ndarray) -> pygame.Surface:
+    """
+    This function is used for 24-32 bit pygame surface with pixel alphas transparency layer
+
+    make_surface(RGBA array) -> Surface
+
+    Argument rgba_array is a 3d numpy array like (width, height, RGBA)
+    This method create a 32 bit pygame surface that combines RGB values and alpha layer.
+
+    :param rgba_array: 3D numpy array created with the method surface.make_array.
+                       Combine RGB values and alpha values.
+    :return:           Return a pixels alpha surface.This surface contains a transparency value
+                       for each pixels.
+    """
+    return pygame.image.frombuffer((rgba_array.transpose(1, 0, 2)).copy(order='C').astype(numpy.uint8),
+                                   (rgba_array.shape[:2][0], rgba_array.shape[:2][1]), 'RGBA').convert_alpha()
+
+
+def blend_texture(surface_, interval_, color_) -> pygame.Surface:
+    """
+    Compatible with 24-32 bit pixel alphas texture.
+    Blend two colors together to produce a third color.
+    Alpha channel of the source image will be transfer to the destination surface (no alteration
+    of the alpha channel)
+
+    :param surface_: pygame surface
+    :param interval_: number of steps or intervals, int value
+    :param color_: Destination color. Can be a pygame.Color or a tuple
+    :return: return a pygame.surface supporting per-pixels transparency only if the surface passed
+                    as an argument has been created with convert_alpha() method.
+                    Pixel transparency of the source array will be unchanged.
+    """
+
+    source_array = pygame.surfarray.pixels3d(surface_)
+    alpha_channel = pygame.surfarray.pixels_alpha(surface_)
+    diff = (numpy.full_like(source_array.shape, color_[:3]) - source_array) * interval_
+    rgba_array = numpy.dstack((numpy.add(source_array, diff), alpha_channel)).astype(dtype=numpy.uint8)
+    return pygame.image.frombuffer((rgba_array.transpose(1, 0, 2)).copy(order='C').astype(numpy.uint8),
+                                   (rgba_array.shape[:2][0], rgba_array.shape[:2][1]), 'RGBA').convert_alpha()
+
+
+# Add transparency value to all pixels including black pixels
+def add_transparency_all(rgb_array: numpy.ndarray, alpha_: numpy.ndarray, value: int) -> pygame.Surface:
+    """
+    Increase transparency of a surface
+    This method is equivalent to pygame.Surface.set_alpha() but conserve the per-pixel properties of a texture
+    All pixels will be update with a new transparency value.
+    If you need to increase transparency on visible pixels only, prefer the method add_transparency instead.
+    :param rgb_array:
+    :param alpha_:
+    :param value:
+    :return:
+    """
+    # if not 0 <= value <= 255:
+    #     raise ERROR('\n[-] invalid value for argument value, should be 0 < value <=255 got %s '
+    #                 % value)
+    # method 1
+    """
+    mask = (alpha_ >= value)
+    mask_zero = (alpha_ < value)
+    alpha_[:][mask_zero] = 0
+    alpha_[:][mask] -= value
+    return make_surface(make_array(rgb_array, alpha_.astype(numpy.uint8)))
+    """
+    # method 2
+    alpha_ = alpha_.astype(numpy.int16)
+    alpha_ -= value
+    numpy.putmask(alpha_, alpha_ < 0, 0)
+
+    return make_surface(make_array(rgb_array, alpha_.astype(numpy.uint8))).convert_alpha()
+
+
 class ERROR(BaseException):
     pass
 
@@ -207,6 +390,9 @@ class Radar(pygame.sprite.Sprite):
 
         self.val = 0
         self.screen = (pygame.display.get_surface()).get_rect()
+        self.pulsating = False              # Default radar is not pulsating
+        self.pulse_time = 70                # time between pulse, 70 frames
+        self.last_pulse = 0                 # time of the last pulse (frame number)
         Radar.inventory.append(self)
 
     @staticmethod
@@ -232,6 +418,11 @@ class Radar(pygame.sprite.Sprite):
     def quit(self):
         Radar.active = False
         self.kill()
+
+    def is_still_loading_up(self):
+        """ Pulsating at regular interval """
+        global FRAME
+        return FRAME - self.last_pulse > self.pulse_time
 
     def update(self):
 
@@ -262,12 +453,18 @@ class Radar(pygame.sprite.Sprite):
 
                 if None not in res:
                     # if not SND.get_identical_id(id(pulse)):
-                    if FRAME % 35 == 0:
+                    if self.is_still_loading_up():
+                        Halo(self.rect, 15, 0)
+                        self.pulsating = True
+                        self.last_pulse = FRAME
                         global MOUSE_POS
                         pulse_array = make_echo(pygame.sndarray.samples(pulse))
                         SND.play(sound_=pulse_array, loop_=False,
                                  priority_=0, volume_=min(1 - distance_, 1), fade_out_ms=0, panning_=True,
                                  name_='PULSE', x_=MOUSE_POS[0], object_id_=id(pulse))
+                    else:
+                        self.pulsating = False
+
                     hrect.center = pygame.math.Vector2(res) / self.ratio
                     hrect.center -= pygame.math.Vector2(dot_w, dot_h) / 2
                     self.image.blit(dot_shape[self.val % len(dot_shape) - 1] if isinstance(dot_shape, list) else
@@ -333,13 +530,21 @@ if __name__ == '__main__':
     freetype.init(cache_size=64, resolution=72)
     SCREENRECT = pygame.Rect(0, 0, 600, 600)
     screen = pygame.display.set_mode(SCREENRECT.size, pygame.HWSURFACE, 32)
-    BACKGROUND = pygame.image.load(GAMEPATH +
-                                   'Assets\\background1.jpg').convert()
-    BACKGROUND = pygame.transform.smoothscale(BACKGROUND, SCREENRECT.size)
+    BACKGROUND = []
+    BACKGROUND1 = pygame.image.load(GAMEPATH +
+                                    'Assets\\Alien_isolation1.jpg').convert()
+    BACKGROUND.append(pygame.transform.smoothscale(BACKGROUND1, SCREENRECT.size))
+    BACKGROUND2 = pygame.image.load(GAMEPATH +
+                                    'Assets\\Alien_isolation2.jpg').convert()
+    BACKGROUND.append(pygame.transform.smoothscale(BACKGROUND2, SCREENRECT.size))
+    BACKGROUND3 = pygame.image.load(GAMEPATH +
+                                    'Assets\\Alien_isolation3.jpg').convert()
+    BACKGROUND.append(pygame.transform.smoothscale(BACKGROUND3, SCREENRECT.size))
+
     FONT = freetype.Font(os.path.join(GAMEPATH + 'Assets\\', 'ARCADE_R.ttf'), size=9)
     FONT.antialiased = True
     clock = pygame.time.Clock()
-    screen.blit(BACKGROUND, (0, 0))
+    screen.blit(BACKGROUND1, (0, 0))
     sprite_group = pygame.sprite.Group()
     GL.All = LayeredUpdatesModified()
     GL.GROUP_UNION = pygame.sprite.Group()
@@ -365,7 +570,7 @@ if __name__ == '__main__':
 
 
     MOUSE_POS = pygame.math.Vector2()
-    Player.images = pygame.transform.rotozoom(pygame.image.load('Assets\\Condor.png'), 180, 0.07)
+    Player.images = pygame.transform.rotozoom(pygame.image.load('Assets\\alien.png'), 0, 0.5)
     Player.containers = GL.All
     Player.mouse_pos = MOUSE_POS
     player = Player()
@@ -387,7 +592,7 @@ if __name__ == '__main__':
 
     aircraft = spread_sheet_fs8(GAMEPATH +
                                 'Assets\\BrightDot_16x16_4x8_red_.png', 16, 2, 8)
-    aircraft = reshape(aircraft, (size, size))
+    aircraft = reshape(aircraft, (size + 20, size + 20))
 
     missile = spread_sheet_fs8(GAMEPATH + 'Assets\\BrightDot_16x16_4x8_purple_.png', 16, 2, 8)
     missile = reshape(missile, (size, size))
@@ -397,6 +602,31 @@ if __name__ == '__main__':
     friend = reshape(friend, (size, size))
 
     Radar.category = {'aircraft': aircraft, 'ground': ground, 'boss': boss, 'missile': missile, 'friend': friend}
+
+    # WHITE HALO
+    HALO_SPRITE = []
+    HALO_SPRITE_ = load_per_pixel('Assets\\WhiteHalo.png')
+    # HALO_SPRITE_ = pygame.image.load('Assets\\WhiteHalo_.png').convert()
+    steps = numpy.array([0., 0.03333333, 0.06666667, 0.1, 0.13333333,
+                         0.16666667, 0.2, 0.23333333, 0.26666667, 0.3,
+                         0.33333333, 0.36666667, 0.4, 0.43333333, 0.46666667,
+                         0.5, 0.53333333, 0.56666667, 0.6, 0.63333333,
+                         0.66666667, 0.7, 0.73333333, 0.76666667, 0.8,
+                         0.83333333, 0.86666667, 0.9, 0.93333333, 0.96666667])
+
+    for number in range(30):
+        # Blend red
+        surface = blend_texture(HALO_SPRITE_, steps[number], pygame.Color(128, 255, 255, 255))
+        rgb = pygame.surfarray.pixels3d(surface)
+        alpha = pygame.surfarray.array_alpha(surface)
+        surface = add_transparency_all(rgb, alpha, int(255 * steps[number] / 8))
+        size = pygame.math.Vector2(surface.get_size())
+        size *= (number / 10)
+        surface1 = pygame.transform.smoothscale(surface, (int(size.x), int(size.y)))
+        HALO_SPRITE.append(surface1)
+
+    Halo.containers = GL.All
+    Halo.images = HALO_SPRITE
 
     pulse = pygame.mixer.Sound('Assets\\pulse.ogg')
     SoundServer.SoundControl.SCREENRECT = SCREENRECT
@@ -409,13 +639,13 @@ if __name__ == '__main__':
 
     rad = Radar(location_=((SCREENRECT.w - Radar_interface[0].get_width() // 2),
                            (SCREENRECT.h - Radar_interface[0].get_height() // 2)),
-                gl_=GL, player_=player, speed_=25, layer_=0)
+                gl_=GL, player_=player, speed_=16, layer_=0)
     rad._blend = pygame.BLEND_RGB_ADD
 
     STOP_GAME = False
 
     FRAME = 0
-
+    index = 0
     while not STOP_GAME:
 
         for event in pygame.event.get():
@@ -431,20 +661,28 @@ if __name__ == '__main__':
             if keys[K_ESCAPE]:
                 STOP_GAME = True
 
+            if keys[K_F8]:
+                pygame.image.save(screen, 'screenshot' + str(FRAME) + '.png')
+
             if event.type == pygame.MOUSEMOTION:
                 MOUSE_POS = pygame.math.Vector2(event.pos)
                 Player.mouse_pos = MOUSE_POS
 
         # screen.fill((0, 0, 0))
-        screen.blit(BACKGROUND, (0, 0))
+        screen.blit(BACKGROUND[index], (0, 0))
+        if FRAME % 800 == 0:
+            if index < len(BACKGROUND) - 1:
+                index += 1
+            else:
+                index = 0
 
         pygame.draw.aaline(screen, (255, 255, 255, 255), SCREENRECT.midtop, SCREENRECT.midbottom, 1)
         pygame.draw.aaline(screen, (255, 255, 255, 255), SCREENRECT.midleft, SCREENRECT.midright, 1)
-        pygame.draw.circle(screen, (255, 0, 0, 0), SCREENRECT.center, 300, 1)
+        pygame.draw.circle(screen, (255, 255, 255, 0), SCREENRECT.center, 300, 1)
 
         GL.All.update()
         GL.All.draw(screen)
-        pygame.draw.circle(screen, (255, 255, 255, 255), SCREENRECT.center, 2)
+
 
         GL.TIME_PASSED_SECONDS = clock.tick(120)
 
